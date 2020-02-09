e17183c4e6965aec5c1be2b1985fcd15452fa1b6
胡伟健 <598307691@qq.com>
2020-02-09 08:37:45 +0800

    引入 enzyme 测试React组件事件

--- test/setupTexts.js
+++ test/setupTexts.js
@@ -4,3 +4,6 @@
  * @Date: 2020-02-09 07:50:26
  * @Desc: 单元测试启动配置
  */
+const Enzyme = require('enzyme')
+const Adapter = require('enzyme-adapter-react-16')
+Enzyme.configure({adapter: new Adapter()})
