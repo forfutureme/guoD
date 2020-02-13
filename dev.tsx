9db016deb5b92bf923dffdef42478bcbea9cd9e7
胡伟健 <598307691@qq.com>
2020-02-13 08:29:26 +0800

    跳整开发示例到use目录,新增Dialog组件

--- dev.tsx
+++ dev.tsx
@@ -12,36 +12,18 @@ import './dev.scss'
 import Hello from '@lib/hello/Hello'
 // import HelloEvent from '@lib/hello/HelloEvent'
 // import HelloImage from '@lib/hello/HelloImage'
-import Icon from '@lib/icon/Icon'
-import Button from '@lib/button/Button'
+import UseIcon from './use/UseIcon'
+import UseButton from './use/UseButton'
+import UseDialog from './use/UseDialog'
 
 ReactDOM.render(
   <div className="main">
     <Hello />
     {/* <HelloEvent /> */}
     {/* <HelloImage /> */}
-    <div className="icon-use">
-      <h3>icon</h3>
-      <div className="icon-area">
-        <Icon name="qq" />
-        <Icon name="loading" />
-        <Icon name="close" style={{color: '#f00'}} />
-      </div>
-    </div>
-    <div className="button-use">
-      <h3>button</h3>
-      <div className="button-area">
-        <Button>默认</Button>
-        <Button
-          theme="primary"
-          onClick={() => {
-            console.log(123)
-          }}>
-          提交
-        </Button>
-        <Button theme="error">Error</Button>
-      </div>
-    </div>
+    <UseIcon />
+    <UseButton />
+    <UseDialog />
   </div>,
   document.getElementById('app')
 )
